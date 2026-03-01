f41685b98c2d68e6aec1b2b7a4c03e4cf2f7634d
Girish Sista <vjgcs@udel.edu>
2026-03-01 00:49:21 -0500

    changed port&host settings

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  const host = process.env.HOST || undefined;
+  const host = process.env.HOST ;
   app.enableCors({
     origin: ['http://localhost:3000', 'http://localhost:3001',"https://recipebuilder-fuc3.onrender.com","recipe-builder.vjgcs.workers.dev" ],
     credentials: true,
